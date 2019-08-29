473f133af611b1793edad4103e1cca5e29346f59
663 <maxrago123@gmail.com>
2019-08-29 06:36:55 -0400

    Started modifying various pages with a more centralized look.

--- static/js/footerInject.js
+++ static/js/footerInject.js
@@ -7,14 +7,14 @@ footerContent = document.createElement('div');
 //footerBarA - For non board pages, used on index.html, faq.html, etc...
 if (footerType == 'A')
 {
-footerContent.innerHTML = '<footer><div class = "footerBar"><a href="/.static/faq.html#faq1">About</a> • <a href="/meta/res/1.html#">Feedback</a> • <a href="/.static/contact.html#legal">Legal</a> • <a href="/.static/contact.html">Contact</a></div><br />Copyright © 2019 16chan Development Team.</footer>';
+footerContent.innerHTML = '<footer><div class = "footerBar"><a href="/.static/pages/about.html">About</a> • <a href="/meta/res/1.html#">Feedback</a> • <a href="/.static/pages/legal.html">Legal</a> • <a href="/.static/pages/contact.html">Contact</a></div><br />Copyright © 2019 16chan Development Team.</footer>';
 }
 
 //footerBarB - For board pages, used on board.html, thread.html, catalog.html, etc...
 else if (footerType == 'B')
 {
 footerContent.innerHTML = 
-'<footer id="footer">All trademarks and copyrights on this page are owned by their respective parties. Images uploaded are the responsibility of the Poster. Comments are owned by the Poster.<br /><br /><div class = "footerBar"><a href="/.static/faq.html#faq1">About</a> • <a href="/meta/res/1.html#">Feedback</a> • <a href="/.static/contact.html#legal">Legal</a> • <a href="/.static/contact.html">Contact</a></div></footer>';
+'<footer id="footer">All trademarks and copyrights on this page are owned by their respective parties. Images uploaded are the responsibility of the Poster. Comments are owned by the Poster.<br /><br /><div class = "footerBar"><a href="/.static/pages/about.html">About</a> • <a href="/meta/res/1.html#">Feedback</a> • <a href="/.static/pages/legal.html">Legal</a> • <a href="/.static/pages/contact.html">Contact</a></div></footer>';
 }
 
 footerBlock = document.getElementById('footerBarType');
